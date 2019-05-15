990493cd92e026d7ecee1e6d707aabcd4729a734
Bulana <moorbulana@gmail.com>
2019-05-15 19:21:23 +0200

    added permissions to add from external storage on manifest file

--- app/src/main/java/com/bulana/mymeme/MainActivity.java
+++ app/src/main/java/com/bulana/mymeme/MainActivity.java
@@ -9,14 +9,32 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private static int RESULT_LOAD_IMAGE;
+
+    TextView topText;
+    TextView bottomText;
+    EditText editTop;
+    EditText editBottom;
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        topText = findViewById(R.id.memeTopText);
+        bottomText = findViewById(R.id.memeBottomText);
+        editTop = findViewById(R.id.editTop);
+        editBottom = findViewById(R.id.editBottom);
+        imageView = findViewById(R.id.memeImage);
     }
 
     public void addImage(View view) {
@@ -25,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tryMeme(View view) {
+        topText.setText(editTop.getText().toString());
+        bottomText.setText(editBottom.getText().toString());
+        hideKeyboard(view);
     }
 
     public void saveImage(View view) {
@@ -33,9 +54,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null!= data) {
+        if (resultCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
             Uri selectedImage = data.getData();
-            String [] filePathColumn = {MediaStore.Images.Media.DATA};
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
             Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
             cursor.moveToFirst();
@@ -44,8 +65,16 @@ public class MainActivity extends AppCompatActivity {
             String picturePath = cursor.getString(columnIndex);
             cursor.close();
 
-            ImageView imageView = (ImageView) findViewById(R.id.memeImage);
+            ImageView imageView = findViewById(R.id.memeImage);
             imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
         }
     }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+    }
 }
+
+
+
