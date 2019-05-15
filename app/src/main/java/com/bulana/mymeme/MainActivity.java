fd4b9a54121947a765a83a644a346851ed891e92
Bulana <moorbulana@gmail.com>
2019-05-15 16:43:19 +0200

    access camera and capture image work

--- app/src/main/java/com/bulana/mymeme/MainActivity.java
+++ app/src/main/java/com/bulana/mymeme/MainActivity.java
@@ -1,11 +1,18 @@
 package com.bulana.mymeme;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int RESULT_LOAD_IMAGE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,11 +20,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addImage(View view) {
+        Intent imageFromGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(imageFromGalleryIntent, RESULT_LOAD_IMAGE);
     }
 
     public void tryMeme(View view) {
     }
 
     public void saveImage(View view) {
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null!= data) {
+            Uri selectedImage = data.getData();
+            String [] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.memeImage);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
     }
 }
